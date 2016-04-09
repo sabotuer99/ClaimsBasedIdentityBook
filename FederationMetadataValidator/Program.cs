74b97ee06ff4ed48716a25220c7a43a7f8177490
sabotuer99 <sabotuer99@gmail.com>
2016-04-09 13:50:11 -0600

    converted sample 6, added console app to validate federation metadata

---
+++ FederationMetadataValidator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FederationMetadataValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var file = new FileStream(@"C:\Users\troy\projects\FederatedDemos\6-FederationAndAcs\Code\Adatum.FederationProvider.6\FederationMetadata\2007-06\FederationMetadata.xml", FileMode.Open))
+            {
+                using (var xmlr = new XmlTextReader(file))
+                {
+                    new System.IdentityModel.Metadata.MetadataSerializer().ReadMetadata(xmlr);
+                }
+            }
+        }
+    }
+}
