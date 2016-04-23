babd1e467f1da9d03aa2c898061d5bde1a51202b
sabotuer99 <sabotuer99@gmail.com>
2016-04-22 22:20:41 -0600

    Got the next couple steps working

---
+++ Shared Code/Samples.Web.ClaimsUtilities/MyCustomToken.cs
@@ -0,0 +1,36 @@
+﻿namespace MyCustomToken
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.IdentityModel.Tokens;
+
+    /// <summary>
+    /// Summary description for MyCustomToken
+    /// </summary>
+    public class MyCustomToken : SecurityToken
+    {
+        public MyCustomToken()
+        {
+        }
+
+        public override string Id
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override ReadOnlyCollection<SecurityKey> SecurityKeys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override DateTime ValidFrom
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override DateTime ValidTo
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Shared Code/Samples.Web.ClaimsUtilities/MyCustomTokenHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace MyCustomToken
+{
+    using System;
+    using Microsoft.IdentityModel.Tokens;
+    using System.Xml;
+
+    /// <summary>
+    /// Summary description for MyCustomTokenHandler
+    /// </summary>
+    public class MyCustomTokenHandler : SecurityTokenHandler
+    {
+        public MyCustomTokenHandler()
+        {
+        }
+
+        public override string[] GetTokenTypeIdentifiers()
+        {
+            return new string[] { "urn:mycustomtoken" };
+        }
+
+        public override Type TokenType
+        {
+            get { return typeof(MyCustomToken); }
+        }
+
+        public override bool CanReadToken(XmlReader reader)
+        {
+            if (reader.LocalName.Equals("MyCustomToken") &&
+                reader.NamespaceURI.Equals("urn:mycustomtoken"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
